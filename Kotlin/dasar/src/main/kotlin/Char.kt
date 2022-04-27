0a6c3c70b6bc3d2853146acf82b43e5d8bcdc9ff
TobiAlbertino <tobialbertino@gmail.com>
2022-04-27 13:48:14 +0700

    kotlin Coursr

---
+++ Kotlin/dasar/src/main/kotlin/Char.kt
@@ -0,0 +1,9 @@
+fun main() {
+  var e: Char = 'E'
+  var k: Char = 'K'
+  var o: Char = 'O'
+
+  print(e)
+  print(k)
+  print(o)
+}
---
+++ Kotlin/dasar/src/main/kotlin/Number.kt
@@ -0,0 +1,22 @@
+fun main() {
+    var age : Byte = 30
+    var number : Int = 200
+
+    println(age)
+    println(number)
+    //    Float
+    var sample : Float = 10.10F
+    println(sample)
+
+    var binary: Int = 0b010101
+    println(binary)
+
+    var price: Long =9_000_000_000L
+    println(price)
+
+    var priceInt: Int = price.toInt()
+    println(priceInt)
+
+    var doubleBinary: Double = binary.toDouble()
+    println(doubleBinary)
+}
