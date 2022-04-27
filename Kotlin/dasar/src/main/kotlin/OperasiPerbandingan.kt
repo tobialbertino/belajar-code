dc3c809951219192a290095f363196db29b54e81
TobiAlbertino <tobialbertino@gmail.com>
2022-04-27 16:03:11 +0700

    update

--- Kotlin/dasar/src/main/kotlin/OperasiPerbandingan.kt
+++ Kotlin/dasar/src/main/kotlin/OperasiPerbandingan.kt
@@ -1,2 +1,16 @@
-class OperasiPerbandingan {
+fun main() {
+    val a = 100
+    val b = 200
+
+    val result: Boolean = a < b
+    println(result)
+
+    println(100 >= 100)
+    println(100 <= 100)
+
+    println("a" < "b")
+
+    println("a" == "a")
+
+    println("a" != "b")
 }
