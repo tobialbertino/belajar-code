83428438c173a3a3ffce9def3ffaf3c4153a22a2
Tobi Albertino <muhamad_tobiaja232@apps.ipb.ac.id>
2023-02-09 22:34:30 +0700

    add notification table

--- SQL/mysql/notification/notification.sql
+++ SQL/mysql/notification/notification.sql
@@ -15,3 +15,38 @@ CREATE TABLE user(
 INSERT INTO user(id, name) VALUES ("tobi", "tobi albertino");
 INSERT INTO user(id, name) VALUES ("tobi2", "tobi albertino2");
 SELECT * FROM user ORDER BY id;
+
+CREATE TABLE notification(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    detail TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    user_id VARCHAR(100),
+
+    PRIMARY KEY (id)
+);
+DESCRIBE notification;
+
+INSERT INTO notification(title, detail, created_at, user_id)
+VALUES ('Contoh Pesanan', 'Detail Pesanan', CURRENT_TIMESTAMP(), 'tobi');
+INSERT INTO notification(title, detail, created_at, user_id)
+VALUES ('Contoh Promo', 'Detail Promo', CURRENT_TIMESTAMP(), NULL);
+INSERT INTO notification(title, detail, created_at, user_id)
+VALUES ('Contoh Pembayaran', 'Detail Pembayaran', CURRENT_TIMESTAMP(), 'tobi2');
+
+SELECT * FROM notification;
+SELECT *
+FROM notification
+WHERE (
+        user_id = 'tobi'
+        OR user_id IS NULL
+    )
+ORDER BY created_at DESC;
+
+SELECT *
+FROM notification
+WHERE (
+        user_id = 'tobi2'
+        OR user_id IS NULL
+    )
+ORDER BY created_at DESC;
