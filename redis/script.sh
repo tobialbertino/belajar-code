7ade05d17839c7f8a3c9cf5af00e182ebe612221
tobialbertino <tobialbertino@gmail.com>
2023-11-20 00:21:48 +0700

    feat: update learn redis

--- redis/script.sh
+++ redis/script.sh
@@ -4,7 +4,10 @@ docker image inspect tobialbertino/redis:v1
 
 docker container create --name redisDocker -p 6379:6379 tobialbertino/redis:v1 
 
-docker container rm redisDocker
 docker container start redisDocker
 docker container logs redisDocker
 docker container stop redisDocker
+docker container rm redisDocker
+
+# Redis pipeline 
+redis-cli -h localhost -p 6379 -n 0 --pipe < input-bulk.txt
