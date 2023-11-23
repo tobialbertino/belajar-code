8263f127617daac10314e3c1e053544e22e40926
tobialbertino <tobialbertino@gmail.com>
2023-11-23 22:33:03 +0700

    feat: learn redis.conf

--- redis/script.sh
+++ redis/script.sh
@@ -9,5 +9,36 @@ docker container logs redisDocker
 docker container stop redisDocker
 docker container rm redisDocker
 
+redis-cli -h 127.0.0.1 -p 6379
+auth tobi secret
+
 # Redis pipeline 
 redis-cli -h localhost -p 6379 -n 0 --pipe < input-bulk.txt
+
+# Redis Transaction, 
+multi # start transaction
+set apple "Apple"
+set samsung "Samsung"
+set xiaomi "Xiaomi"
+exec # commit transaction
+
+# Redis Transaction, discard
+multi # start transaction
+set apple "Apple"
+set samsung "Samsung"
+set xiaomi "Xiaomi"
+discard # rollback transaction
+
+# Redis Monitor 
+monitor # Listen for all request received by the server in real time
+
+# Redis Server Information
+info # Get information and statistics about the server
+config get <key> # Get the value of a configuration parameter from redis.conf
+
+# Redis Client Connection
+client list # get the list og client connections
+client id # Returns the client ID for the current connection
+client kill ip:port # Kill the connection of a client
+
+
