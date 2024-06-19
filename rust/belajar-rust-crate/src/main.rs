5758521cbfa9818d8c97d78e3aced6141524d460
tobialbertino <tobialbertino@gmail.com>
2024-06-19 23:31:46 +0700

    feat: upgrade library

--- rust/belajar-rust-crate/src/main.rs
+++ rust/belajar-rust-crate/src/main.rs
@@ -1,9 +1,15 @@
-use say_hello_tobialbertino::{say_hello, say_goodbye};
+use say_hello_tobialbertino::{say_hello, say_goodbye, say_hello_to_everyone, say_goodbye_to_everyone};
 
 fn main() {
     let response = say_hello("Tobi");
     println!("{}", response);
 
     let response = say_goodbye("Tobi");
+    println!("{}", response);
+
+    let response = say_hello_to_everyone();
+    println!("{}", response);
+
+    let response = say_goodbye_to_everyone();
     println!("{}", response);
 }
