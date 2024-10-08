8033453e57b952d49045b4449d7f0d3083107e00
tobialbertino <tobialbertino@gmail.com>
2024-10-08 10:25:34 +0700

    feat: add post method

--- rust/simple-api/src/main.rs
+++ rust/simple-api/src/main.rs
@@ -1,14 +1,27 @@
-use axum::{
-    routing::get,
-    Router,
-};
+pub mod routes;
+
+use axum::routing::post;
+use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    // build our application with a route
+    let app = Router::new()
+        .route("/", get(routes::handler))
+        .route("/test", get(routes::test_handler))
+        .route("/test", post(routes::post_handler));
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    // Bind the server to the configured port.
+    let listener = match tokio::net::TcpListener::bind(format!("0.0.0.0:{}", 3000)).await {
+        Ok(listener) => listener,
+        Err(err) => {
+            eprintln!("Failed to bind to port {}: {:?}", 3000, err);
+            return;
+        }
+    };
+
+    // Start the server.
+    if let Err(err) = axum::serve(listener, app).await {
+        eprintln!("Failed to start server: {:?}", err);
+    }
 }
