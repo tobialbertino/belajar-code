d5e7576988eb227143353014604781b5af2ea4b6
tobialbertino <tobialbertino@gmail.com>
2025-01-01 21:42:31 +0700

    feat: add learn rust date time

--- rust/belajar-rust-datetime/src/main.rs
+++ rust/belajar-rust-datetime/src/main.rs
@@ -5,9 +5,10 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use chrono::{
-        Datelike, Duration, FixedOffset, NaiveDate, NaiveDateTime, NaiveTime, TimeZone, Timelike,
+        DateTime, Datelike, Duration, FixedOffset, Local, NaiveDate, NaiveDateTime, NaiveTime,
+        TimeZone, Timelike, Utc,
     };
-    use chrono_tz::Asia;
+    use chrono_tz::{Asia, Tz};
 
     #[test]
     fn test_date() {
@@ -81,5 +82,44 @@ mod tests {
         println!("{}", asia_jakarta_date_time);
     }
 
+    #[test]
+    fn test_date_time_with_time_zone() {
+        let utc_date_time: DateTime<Utc> = Utc::now();
+        let asia_jakarta_date_time: DateTime<Tz> =
+            Asia::Jakarta.from_utc_datetime(&utc_date_time.naive_utc());
 
+        println!("{}", utc_date_time);
+        println!("{}", asia_jakarta_date_time);
+
+        let local_date_time: DateTime<Local> = Local::now();
+        let asia_jakarta_date_time: DateTime<Tz> = Asia::Jakarta
+            .from_local_datetime(&local_date_time.naive_local())
+            .unwrap();
+
+        println!("{}", local_date_time);
+        println!("{}", asia_jakarta_date_time);
+    }
+
+    #[test]
+    fn test_parsing() {
+        let string = String::from("2024-12-31 10:10:10 +07:00");
+        let time = DateTime::parse_from_str(&string, "%Y-%m-%d %H:%M:%S %:z").unwrap();
+
+        println!("{}", time.year());
+        println!("{}", time.month());
+        println!("{}", time.day());
+        println!("{}", time.hour());
+        println!("{}", time.minute());
+        println!("{}", time.second());
+        println!("{}", time.nanosecond());
+        println!("{}", time.timezone());
+    }
+
+    #[test]
+    fn test_format() {
+        let time = Local::now();
+        let string = time.format("%Y-%m-%d %H:%M:%S %:z").to_string();
+
+        println!("{}", string);
+    }
 }
