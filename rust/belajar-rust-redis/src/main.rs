c1df4149042d3e0dbc8954d8de46ff052dfda665
tobialbertino <tobialbertino@gmail.com>
2025-01-13 00:02:52 +0700

    feat: add lear rust radis

--- rust/belajar-rust-redis/src/main.rs
+++ rust/belajar-rust-redis/src/main.rs
@@ -4,9 +4,69 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use std::num::NonZero;
     use redis::aio::MultiplexedConnection;
     use redis::{AsyncCommands, Client, Commands, RedisError};
     use std::time::Duration;
+
+    #[tokio::test]
+    async fn test_sorted_set() -> Result<(), RedisError> {
+        let mut con = get_client().await?;
+        let _: () = con.zadd("sorted_set", "value1", 100).await?;
+        let _: () = con.zadd("sorted_set", "value2", 90).await?;
+        let _: () = con.zadd("sorted_set", "value3", 80).await?;
+        // sorted set is sorted by score
+
+        let len: i32 = con.zcard("sorted_set").await?;
+        assert_eq!(3, len);
+
+        let sets: Vec<String> = con.zrange("sorted_set", 0, -1).await?;
+        assert_eq!(vec!["value3", "value2", "value1"], sets);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_set() -> Result<(), RedisError> {
+        let mut con = get_client().await?;
+        let _: () = con.sadd("set", "value1").await?; // always unique
+        let _: () = con.sadd("set", "value1").await?;
+        let _: () = con.sadd("set", "value2").await?;
+        let _: () = con.sadd("set", "value2").await?;
+        let _: () = con.sadd("set", "value3").await?;
+        let _: () = con.sadd("set", "value3").await?;
+
+        let len: i32 = con.scard("set").await?;
+        assert_eq!(3, len);
+
+        let sets: Vec<String> = con.smembers("set").await?;
+        assert_eq!(vec!["value1", "value2", "value3"], sets);
+
+        Ok(())
+    }
+
+   #[tokio::test]
+    async fn test_list() -> Result<(), RedisError> {
+        let mut con = get_client().await?;
+        let _: () = con.rpush("list", "value1").await?;
+        let _: () = con.rpush("list", "value2").await?;
+        let _: () = con.rpush("list", "value3").await?;
+
+        let len: i32 = con.llen("list").await?;
+        assert_eq!(3, len);
+
+        let lists: Vec<String> = con.lrange("list", 0, -1).await?; // -1 means all elements
+        assert_eq!(vec!["value1", "value2", "value3"], lists);
+
+        let lists: Vec<String> = con.lpop("list",NonZero::new(1)).await?;
+        assert_eq!(vec!["value1"], lists);
+        let lists: Vec<String> = con.lpop("list",NonZero::new(1)).await?;
+        assert_eq!(vec!["value2"], lists);
+        let lists: Vec<String> = con.lpop("list",NonZero::new(1)).await?;
+        assert_eq!(vec!["value3"], lists);
+
+        Ok(())
+    }
 
     #[tokio::test]
     async fn test_string() -> Result<(), RedisError> {
