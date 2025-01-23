e68c99f442fa3c2fe757067d17fd3f4d0ad88eb2
tobialbertino <tobialbertino@gmail.com>
2025-01-23 21:30:24 +0700

    feat: add learn rust logging

--- rust/belajar-rust-logging/src/main.rs
+++ rust/belajar-rust-logging/src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use log::{debug, error, info, trace, warn};
+
+    #[test]
+    fn test_log() {
+        log4rs::init_file("./src/env.yaml", Default::default()).unwrap();
+
+        error!("This is an error message");
+        warn!("This is a warning message");
+        info!("This is an info message");
+        debug!("This is a debug message");
+        trace!("This is a trace message");
+    }
+}
+
+#[cfg(test)]
+mod tests2 {
+    use log::{debug, error, info, trace, warn};
+
+    #[test]
+    fn test_log() {
+        log4rs::init_file("./src/env.yaml", Default::default()).unwrap();
+
+        error!("This is an error message");
+        warn!("This is a warning message");
+        info!("This is an info message");
+        debug!("This is a debug message");
+        trace!("This is a trace message");
+    }
+}
