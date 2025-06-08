c07b307c78c022b936e5cf18a7b3aeb1bf300bde
tobialbertino <tobialbertino@gmail.com>
2025-06-08 23:26:44 +0700

    feat: learn axum

--- rust/belajar-axum-web/src/main.rs
+++ rust/belajar-axum-web/src/main.rs
@@ -1,5 +1,6 @@
 use axum::{serve, Router};
-use axum::routing::get;
+use axum::routing::{get, post};
+use axum_test::TestServer;
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -12,4 +13,36 @@ async fn main() {
     serve(listener, app)
         .await
         .unwrap();
+}
+
+#[tokio::test]
+async fn test_axum() {
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, world!" }));
+
+    let server = TestServer::new(app).unwrap();
+    let response = server.get("/").await;
+
+    response.assert_status_ok();
+    response.assert_text("Hello, world!");
+}
+
+#[tokio::test]
+async fn test_method_routing() {
+    async fn hello_world() -> String {
+        "Hello, world!".to_string() 
+    }
+
+    let app = Router::new()
+        .route("/get", get(hello_world))
+        .route("/post", post(hello_world));
+
+    let server = TestServer::new(app).unwrap();
+    let response = server.get("/get").await;
+    response.assert_status_ok();
+    response.assert_text("Hello, world!");
+    
+    let response = server.post("/post").await;
+    response.assert_status_ok();
+    response.assert_text("Hello, world!");
 }
